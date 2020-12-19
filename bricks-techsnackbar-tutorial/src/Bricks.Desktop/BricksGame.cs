4f29d5aafddaed7c80509fbca22e9f7101cbcaff
iivchenko <iivchenko@live.com>
2020-12-19 17:42:19 +0100

    Bricks Tutorial. Refactoring - Introduce state machine for the Game

--- bricks-techsnackbar-tutorial/src/Bricks.Desktop/BricksGame.cs
+++ bricks-techsnackbar-tutorial/src/Bricks.Desktop/BricksGame.cs
@@ -23,7 +23,6 @@ namespace Bricks.Desktop
         private int _screenHeight = 0;
         private MouseState _oldMouseState;
         private KeyboardState _oldKeyboardState;
-        private bool readyToServeBall = true;
         private int ballsRemaining = 3;
 
         // Content
@@ -40,9 +39,12 @@ namespace Bricks.Desktop
 
         private SpriteFont _labelFont;
 
+        private IGameState _state;
+
         public BricksGame()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _state = new InitializeNewGameState(this);
         }
 
         protected override void Initialize()
@@ -58,45 +60,6 @@ namespace Bricks.Desktop
             _graphics.PreferredBackBufferWidth = _screenWidth = 502;
             _graphics.PreferredBackBufferHeight = _screenHeight = 700;
             _graphics.ApplyChanges();
-
-            _paddle = new Paddle(
-                new Vector2(
-                    (_screenWidth - _paddleSprite.Width) / 2,
-                    _screenHeight - 100),
-                _screenWidth,
-                _spriteBatch,
-                _paddleSprite);
-
-            _wall = CreateWall(1, 50, _brickSprite, _spriteBatch);
-            _gameBorder = new GameBorder(_screenWidth, _screenHeight, _spriteBatch, _piexel);
-
-            _ball =
-                new Ball(
-                    _screenWidth,
-                    _screenHeight,
-                    _spriteBatch,
-                    _ballSprite,
-                    _startSound,
-                    _wallBounceSound,
-                    _paddleBounceSound,
-                    _brickSound);
-
-            _staticBall =
-                new Ball(
-                    _screenWidth,
-                    _screenHeight,
-                    _spriteBatch,
-                    _ballSprite,
-                    null,
-                    null,
-                    null,
-                    null)
-                {
-                    X = 25,
-                    Y = 25,
-                    Visible = true,
-                    UseRotation = false
-                };
         }
 
         protected override void LoadContent()
@@ -115,125 +78,24 @@ namespace Bricks.Desktop
             _labelFont = Content.Load<SpriteFont>("Fonts/Arial20");
         }
 
-        private void ServeBall()
-        {
-            if (ballsRemaining < 1)
-            {
-                ballsRemaining = 3;
-                _ball.Score = 0;
-                _wall = CreateWall(1, 50, _brickSprite, _spriteBatch);
-            }
-
-            readyToServeBall = false;
-            float ballX = _paddle.Position.X + (_paddle.Width) / 2;
-            float ballY = _paddle.Position.Y - _ball.Height;
-            _ball.Launch(ballX, ballY, -3, -3);
-        }
-
         protected override void Update(GameTime gameTime)
         {
             if (!IsActive)
             {
-                return;  //our window is not active don't update
+                return;
             }
 
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            var newKeyboardState = Keyboard.GetState();
-            var newMouseState = Mouse.GetState();
-
-            //process mouse move
-            if (_oldMouseState.X != newMouseState.X)
-            {
-                if (newMouseState.X >= 0 || newMouseState.X < _screenWidth)
-                {
-                    _paddle.MoveTo(newMouseState.X);
-                }
-            }
-
-            //process left-click
-            if (newMouseState.LeftButton == ButtonState.Released && _oldMouseState.LeftButton == ButtonState.Pressed && _oldMouseState.X == newMouseState.X && _oldMouseState.Y == newMouseState.Y && readyToServeBall)
-            {
-                ServeBall();
-            }
-
-            //process keyboard events
-            if (newKeyboardState.IsKeyDown(Keys.Left))
-            {
-                _paddle.MoveLeft();
-            }
-            if (newKeyboardState.IsKeyDown(Keys.Right))
-            {
-                _paddle.MoveRight();
-            }
-            if (_oldKeyboardState.IsKeyUp(Keys.Space) && newKeyboardState.IsKeyDown(Keys.Space) && readyToServeBall)
-            {
-                ServeBall();
-            }
-
-            _oldMouseState = newMouseState; // this saves the old state
-            _oldKeyboardState = newKeyboardState;
+            _state.Update(gameTime);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
-
-            // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-
-            _paddle.Draw();
-            foreach (var brick in _wall) brick.Draw();
-
-            _gameBorder.Draw();
-
-            if (_ball.Visible)
-            {
-                bool inPlay = _ball.Move(_wall, _paddle);
-                if (inPlay)
-                {
-                    _ball.Draw();
-                }
-                else
-                {
-                    ballsRemaining--;
-                    readyToServeBall = true;
-                }
-            }
-
-            _staticBall.Draw();
-
-            string scoreMsg = "Score: " + _ball.Score.ToString("00000");
-            Vector2 space = _labelFont.MeasureString(scoreMsg);
-            _spriteBatch.DrawString(_labelFont, scoreMsg, new Vector2((_screenWidth - space.X) / 2, _screenHeight - 40), Color.White);
-            if (_ball.bricksCleared >= 70)
-            {
-                _ball.Visible = false;
-                _ball.bricksCleared = 0;
-                _wall = CreateWall(1, 50, _brickSprite, _spriteBatch);
-                readyToServeBall = true;
-            }
-            if (readyToServeBall)
-            {
-                if (ballsRemaining > 0)
-                {
-                    string startMsg = "Press <Space> or Click Mouse to Start";
-                    Vector2 startSpace = _labelFont.MeasureString(startMsg);
-                    _spriteBatch.DrawString(_labelFont, startMsg, new Vector2((_screenWidth - startSpace.X) / 2, _screenHeight / 2), Color.White);
-                }
-                else
-                {
-                    string endMsg = "Game Over";
-                    Vector2 endSpace = _labelFont.MeasureString(endMsg);
-                    _spriteBatch.DrawString(_labelFont, endMsg, new Vector2((_screenWidth - endSpace.X) / 2, _screenHeight / 2), Color.White);
-                }
-            }
-            _spriteBatch.DrawString(_labelFont, ballsRemaining.ToString(), new Vector2(40, 10), Color.White);
-
-            _spriteBatch.End();
+            _state.Draw(gameTime);
 
             base.Draw(gameTime);
         }
@@ -271,6 +133,235 @@ namespace Bricks.Desktop
             }
 
             return wall.OfType<Brick>().ToList();
+        }
+
+        private interface IGameState
+        {
+            void Update(GameTime gameTime);
+
+            void Draw(GameTime gameTime);
+        }
+
+        private sealed class InitializeNewGameState : IGameState
+        {
+            private readonly BricksGame _game;
+
+            public InitializeNewGameState(BricksGame game)
+            {
+                _game = game;
+            }
+
+            public void Draw(GameTime gameTime)
+            {
+            }
+
+            public void Update(GameTime gameTime)
+            {
+                _game._paddle = new Paddle(
+                new Vector2(
+                    (_game._screenWidth - _game._paddleSprite.Width) / 2,
+                    _game._screenHeight - 100),
+                _game._screenWidth,
+                _game._spriteBatch,
+                _game._paddleSprite);
+
+                _game._wall = CreateWall(1, 50, _game._brickSprite, _game._spriteBatch);
+                _game._gameBorder = new GameBorder(_game._screenWidth, _game._screenHeight, _game._spriteBatch, _game._piexel);
+
+                _game._ball =
+                    new Ball(
+                        _game._screenWidth,
+                        _game._screenHeight,
+                        _game._spriteBatch,
+                        _game._ballSprite,
+                        _game._startSound,
+                        _game._wallBounceSound,
+                        _game._paddleBounceSound,
+                        _game._brickSound);
+
+                _game._staticBall =
+                    new Ball(
+                        _game._screenWidth,
+                        _game._screenHeight,
+                        _game._spriteBatch,
+                        _game._ballSprite,
+                        null,
+                        null,
+                        null,
+                        null)
+                    {
+                        X = 25,
+                        Y = 25,
+                        Visible = true,
+                        UseRotation = false
+                    };
+
+                _game._state = new GameServeBallState(_game);
+            }
+        }
+
+        private sealed class GamePlayState : IGameState
+        {
+            private readonly BricksGame _game;
+
+            public GamePlayState(BricksGame game)
+            {
+                _game = game;
+            }
+
+            public void Draw(GameTime gameTime)
+            {
+                _game.GraphicsDevice.Clear(Color.Black);
+
+                _game._spriteBatch.Begin();
+
+                _game._paddle.Draw();
+                foreach (var brick in _game._wall) brick.Draw();
+
+                _game._gameBorder.Draw();
+
+                _game._ball.Draw();
+
+                _game._staticBall.Draw();
+
+                string scoreMsg = "Score: " + _game._ball.Score.ToString("00000");
+                Vector2 space = _game._labelFont.MeasureString(scoreMsg);
+                _game._spriteBatch.DrawString(_game._labelFont, scoreMsg, new Vector2((_game._screenWidth - space.X) / 2, _game._screenHeight - 40), Color.White);
+
+                _game._spriteBatch.DrawString(_game._labelFont, _game.ballsRemaining.ToString(), new Vector2(40, 10), Color.White);
+
+                _game._spriteBatch.End();
+            }
+
+            public void Update(GameTime gameTime)
+            {
+                if (_game._ball.bricksCleared >= 70)
+                {
+                    _game._state = new InitializeNewGameState(_game);
+                    return;
+                }
+
+                bool inPlay = _game._ball.Move(_game._wall, _game._paddle);
+                if (!inPlay)
+                {
+                    _game.ballsRemaining--;
+                    _game._state = _game.ballsRemaining < 1
+                        ? new GameOverState(_game) as IGameState
+                        : new GameServeBallState(_game) as IGameState;
+                }
+
+                var newKeyboardState = Keyboard.GetState();
+                var newMouseState = Mouse.GetState();
+
+                if (_game._oldMouseState.X != newMouseState.X)
+                {
+                    if (newMouseState.X >= 0 || newMouseState.X < _game._screenWidth)
+                    {
+                        _game._paddle.MoveTo(newMouseState.X);
+                    }
+                }
+
+                if (newKeyboardState.IsKeyDown(Keys.Left))
+                {
+                    _game._paddle.MoveLeft();
+                }
+                if (newKeyboardState.IsKeyDown(Keys.Right))
+                {
+                    _game._paddle.MoveRight();
+                }
+
+                _game._oldMouseState = newMouseState;
+                _game._oldKeyboardState = newKeyboardState;
+            }
+        }
+
+        private sealed class GameServeBallState : IGameState
+        {
+            private const string StartMsg = "Press <Space> or Click Mouse to Start";
+            private readonly BricksGame _game;
+
+            public GameServeBallState(BricksGame game)
+            {
+                _game = game;
+            }
+
+            public void Draw(GameTime gameTime)
+            {
+                _game._spriteBatch.Begin();
+
+                Vector2 startSpace = _game._labelFont.MeasureString(StartMsg);
+                _game._spriteBatch.DrawString(_game._labelFont, StartMsg, new Vector2((_game._screenWidth - startSpace.X) / 2, _game._screenHeight / 2), Color.White);
+
+                _game._spriteBatch.End();
+            }
+
+            public void Update(GameTime gameTime)
+            {
+                var newKeyboardState = Keyboard.GetState();
+                var newMouseState = Mouse.GetState();
+
+                if (
+                    newMouseState.LeftButton == ButtonState.Released && 
+                    _game._oldMouseState.LeftButton == ButtonState.Pressed && 
+                    _game._oldMouseState.X == newMouseState.X && 
+                    _game._oldMouseState.Y == newMouseState.Y)
+                {
+                    float ballX = _game._paddle.Position.X + (_game._paddle.Width) / 2;
+                    float ballY = _game._paddle.Position.Y - _game._ball.Height;
+                    _game._ball.Launch(ballX, ballY, -3, -3);
+
+                    _game._state = new GamePlayState(_game);
+                }
+
+                _game._oldMouseState = newMouseState;
+                _game._oldKeyboardState = newKeyboardState;
+            }
+        }
+
+        private sealed class GameOverState : IGameState
+        {
+            private const string GameOverMessage = "Game Over";
+            private readonly BricksGame _game;
+
+            public GameOverState(BricksGame game)
+            {
+                _game = game;
+            }
+
+            public void Draw(GameTime gameTime)
+            {
+                _game.GraphicsDevice.Clear(Color.Black);
+
+                _game._spriteBatch.Begin();
+
+                var endSpace = _game._labelFont.MeasureString(GameOverMessage);
+                _game._spriteBatch.DrawString(
+                    _game._labelFont,
+                    GameOverMessage,
+                    new Vector2((_game._screenWidth - endSpace.X) / 2, _game._screenHeight / 2),
+                    Color.White);
+
+                _game._spriteBatch.End();
+            }
+
+            public void Update(GameTime gameTime)
+            {
+                var newKeyboardState = Keyboard.GetState();
+                var newMouseState = Mouse.GetState();
+
+                if (
+                    newMouseState.LeftButton == ButtonState.Released &&
+                    _game._oldMouseState.LeftButton == ButtonState.Pressed &&
+                    _game._oldMouseState.X == newMouseState.X &&
+                    _game._oldMouseState.Y == newMouseState.Y)
+                {
+
+                    _game._state = new InitializeNewGameState(_game);
+                }
+
+                _game._oldMouseState = newMouseState;
+                _game._oldKeyboardState = newKeyboardState;
+            }
         }
     }
 }
