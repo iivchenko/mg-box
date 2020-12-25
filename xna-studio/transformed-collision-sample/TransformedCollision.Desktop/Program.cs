926eb94f2f4ff031fccde21316aa09aef744b237
iivchenko <iivchenko@live.com>
2020-12-25 16:55:03 +0100

    XNA Studio. Add Transformed Collision Sample

---
+++ xna-studio/transformed-collision-sample/TransformedCollision.Desktop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TransformedCollision.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new TransformedCollisionGame();
+            
+            game.Run();
+        }
+    }
+}
---
+++ xna-studio/transformed-collision-sample/TransformedCollisionTest.Desktop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TransformedCollisionTest.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new TransformedCollisionTestGame();
+            
+            game.Run();
+        }
+    }
+}
