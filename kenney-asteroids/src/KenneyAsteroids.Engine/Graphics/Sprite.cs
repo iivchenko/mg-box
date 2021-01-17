c9ceb17b4a3bfef366b47b59d96672280cc372bb
iivchenko <iivchenko@live.com>
2021-01-17 11:17:36 +0100

    Kenney Asteroids. Extend engine: add Sprite and SpriteBatchExtensions.

---
+++ kenney-asteroids/src/KenneyAsteroids.Engine/Graphics/Sprite.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KenneyAsteroids.Engine.Graphics
+{
+    public sealed class Sprite
+    {
+        private readonly Texture2D _texture;
+        private readonly Rectangle? _sourceRectangle;
+
+        public Sprite(Texture2D texture, Rectangle? sourceRectangle)
+        {
+            _texture = texture;
+            _sourceRectangle = sourceRectangle;
+
+            if (_sourceRectangle.HasValue)
+            {
+                Height = _sourceRectangle.Value.Height;
+                Width = _sourceRectangle.Value.Width;
+            }
+            else
+            {
+                Height = texture.Height;
+                Width = texture.Width;
+            }
+        }
+
+        public Sprite(Texture2D texture)
+            : this(texture, null)
+        {
+        }
+
+        public float Height { get; }
+        public float Width { get; }
+        internal Texture2D Texture => _texture;
+        internal Rectangle? SourceRectangle => _sourceRectangle;
+    }
+}
---
+++ kenney-asteroids/src/KenneyAsteroids.Engine/Graphics/SpriteBatchExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KenneyAsteroids.Engine.Graphics
+{
+    public static class SpriteBatchExtensions
+    {
+        public static void Draw(
+            this SpriteBatch spriteBatch, 
+            Sprite sprite, 
+            Vector2 position, 
+            Vector2 origin,
+            Vector2 scale,
+            float rotation,
+            Color color,
+            SpriteEffects effect)
+        {
+            spriteBatch
+                .Draw(
+                    sprite.Texture,
+                    position,
+                    sprite.SourceRectangle,
+                    color,
+                    rotation,
+                    origin,
+                    scale,
+                    effect,
+                    0);
+        }
+    }
+}
