a2278d9792457535a3ef945389e738171b91ac82
iivchenko <iivchenko@live.com>
2021-01-17 11:18:22 +0100

    Kenney Asteroids. Extend engine: add EnumerableExtensions for entities collection and add GameTimeExtensions for easy delta retrieval.

---
+++ kenney-asteroids/src/KenneyAsteroids.Engine/EnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using KenneyAsteroids.Engine.Worlds;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KenneyAsteroids.Engine
+{
+    public static class EnumerableExtensions
+    {
+        public static void Iter<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach(var item in source)
+            {
+                action(item);
+            }
+        }
+
+        public static void Update<TEntity>(this IEnumerable<TEntity> entities, GameTime gameTime)
+            where TEntity : Entity
+        {
+            foreach(var entity in entities.Where(x => x is IUpdatable).Cast<IUpdatable>())
+            {
+                entity.Update(gameTime);
+            }
+        }
+
+        public static void Draw<TEntity>(this IEnumerable<TEntity> entities, GameTime gameTime)
+            where TEntity : Entity
+        {
+            foreach (var entity in entities.Where(x => x is IDrawable).Cast<IDrawable>())
+            {
+                entity.Draw(gameTime);
+            }
+        }
+    }
+}
---
+++ kenney-asteroids/src/KenneyAsteroids.Engine/GameTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace KenneyAsteroids.Engine
+{
+    public static class GameTimeExtensions
+    {
+        public static float ToDelta(this GameTime gameTime)
+        {
+            return (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
