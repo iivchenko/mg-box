968135e805fa1c4ff954678aaad4c333a1d58f66
iivchenko <iivchenko@live.com>
2021-01-17 11:17:10 +0100

    Kenney Asteroids. Extend engine: add IUpdatable and IDrawable.

---
+++ kenney-asteroids/src/KenneyAsteroids.Engine/IDrawable.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace KenneyAsteroids.Engine
+{
+    public interface IDrawable
+    {
+        void Draw(GameTime gameTime);
+    }
+}
---
+++ kenney-asteroids/src/KenneyAsteroids.Engine/IUpdatable.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace KenneyAsteroids.Engine
+{
+    public interface IUpdatable
+    {
+        void Update(GameTime gameTime);
+    }
+}
